19c4606ec13a391e74021bb6b4a1bff1e666fc19
Takuma Umezawa <cwan.oss@gmail.com>
2019-08-05 17:14:36 +0900

    fix: #7

--- src/test/suite/utils.test.ts
+++ src/test/suite/utils.test.ts
@@ -10,6 +10,7 @@ suite('utils Test Suite', () => {
     assert.equal('\\u30421\\u30442\\u30463\\u30484\\u304a', utils.nativeToAscii('あ1い2う3え4お'));
     assert.equal('\\ud840\\udc0b', utils.nativeToAscii('𠀋'));
     assert.equal('\\uD840\\uDC0B\\u304A', utils.nativeToAscii('𠀋お', false));
+    assert.equal('\\u00a1 \\u00ff', utils.nativeToAscii('¡ ÿ'));
   });
 
 	test('asciiToNative test', () => {
@@ -19,5 +20,6 @@ suite('utils Test Suite', () => {
     assert.equal('あ1い2う3え4お', utils.asciiToNative('\\u30421\\u30442\\u30463\\u30484\\u304a'));
     assert.equal('𠀋', utils.asciiToNative('\\ud840\\udc0b'));
     assert.equal('𠀋お', utils.asciiToNative('\\uD840\\uDC0B\\u304A'));
+    assert.equal('¡ ÿ', utils.asciiToNative('\\u00a1 \\u00ff'));
   });
 });
